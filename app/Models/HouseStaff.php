ac2ab201fc6f665f107af20024e24d2fddb6b647
FougnigueOuattara <Karimouattara122@gmail.com>
2023-06-03 23:10:34 +0000

    Home page modify

--- app/Models/HouseStaff.php
+++ app/Models/HouseStaff.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HouseStaff extends Model
 {
     use HasFactory;
+    public function houseStaffs():BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+    <h1>{{$test}} les composants blade de laravel</h1>
+</div>
